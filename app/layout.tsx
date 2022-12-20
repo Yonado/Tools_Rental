e9ae3cae1643b631b3fcc6a360a89d55f30f5e39
Yonado <yonatan.doanis@gmail.com>
2022-12-20 12:54:38 +0200

    bg white

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
                 <head>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 </head>
-                <body>
+                <body className="bg-white">
                     <Header />
                     <div className={inter.className}>
                         {children}
