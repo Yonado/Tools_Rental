30e0d8ef1d5e1761ec10ac3e8a6b20c499d4e534
Yonado <yonatan.doanis@gmail.com>
2022-12-20 12:43:15 +0200

    viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
     return (
         <>
             <html>
-                <head />
+                <head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                </head>
                 <body>
                     <Header />
                     <div className={inter.className}>
